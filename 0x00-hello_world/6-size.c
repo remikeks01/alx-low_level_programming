bc2d0f1e4c76cad36864030ce415bf58c1b43220
remikeks <keksclusive@gmail.com>
2022-04-07 16:54:42 -0700

    C program to determine size of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-	printf("Size of a double: %lu byte(s)\n", sizeof(double));
-	printf("Size of a long double: %lu byte(s)", sizeof(long double));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	printf("Size of a double: %ld byte(s)\n", sizeof(double));
 	return (0);
 }
