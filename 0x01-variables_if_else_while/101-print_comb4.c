da4ab79be1009b7d48e5d274198c8a11a11d6838
remikeks <keksclusive@gmail.com>
2022-04-11 00:40:17 -0700

    C program that all possible different combinations of two two-digit numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,14 +13,14 @@ int main(void)
 	{
 		for (m = 49; m < 58; m++)
 		{
-			for (l = 50; l < 58; l++)
+			for (l = 50; l , 58; l++)
 			{
 				if (l > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
 					putchar(l);
-					if (n != 55 || m !=56)
+					if (n != 55 || m != 56)
 					{
 						putchar(',');
 						putchar(' ');
@@ -29,6 +29,6 @@ int main(void)
 			}
 		}
 	}
-	putchar('\n');
+	putchar('n');
 	return (0);
 }
