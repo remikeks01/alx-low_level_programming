ea1b3ad08f322ede99046697e13e7fe3dad11c13
remikeks <keksclusive@gmail.com>
2022-04-11 00:44:50 -0700

    C program that all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,7 +13,7 @@ int main(void)
 	{
 		for (m = 49; m < 58; m++)
 		{
-			for (l = 50; l , 58; l++)
+			for (l = 50; l < 58; l++)
 			{
 				if (l > m && m > n)
 				{
@@ -29,6 +29,6 @@ int main(void)
 			}
 		}
 	}
-	putchar('n');
+	putchar('\n');
 	return (0);
 }
