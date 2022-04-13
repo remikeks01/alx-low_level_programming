fa3485c781a751f68faf6a8efa7907a1a255cdab
remikeks <keksclusive@gmail.com>
2022-04-13 14:10:32 -0700

    C function that prints 10 times the alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * function to alphabets
+ *
+ * Return: void
+ */
+void print_alphabet()
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
