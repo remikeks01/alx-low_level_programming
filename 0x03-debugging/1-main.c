cd7dca8e1ea625214730ed9d37aaf1e202201220
remikeks <keksclusive@gmail.com>
2022-04-14 12:59:11 -0700

    commenting out a bug

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 /*
- * while (i < 10)
- * {
- * 	putchar(i);
- * }
+ *while (i < 10)
+ *{
+ *	 	putchar(i);
+ *}
  */
 printf("Indefinite loop avoided! \\o/\n");
 
