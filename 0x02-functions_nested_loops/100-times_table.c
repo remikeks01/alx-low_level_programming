c6eba9bb9a3730a6a4e78f0dc1916d9fb6b79af8
remikeks <keksclusive@gmail.com>
2022-04-14 06:36:33 -0700

    C function that prints the n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,6 +26,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar(product % 10 + '0');
 				}
 				else if (product >= 10 && product < 100)
