17b277cc1cdd9510f58f9cacbef8c67e913a8cd4
remikeks <keksclusive@gmail.com>
2022-04-14 12:35:17 -0700

    file to test a function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Tests function for positive or negative
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
