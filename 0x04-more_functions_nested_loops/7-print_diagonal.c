dccb6602ac127de56f27b2535a9ce8040ab307ef
remikeks <keksclusive@gmail.com>
2022-04-15 01:57:13 -0700

    function that draws a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,12 +15,16 @@ void print_diagonal(int n)
 		for (; i < n; i++)
 		{
 			for(j = 0; j < i; j++)
+			{
 				_putchar(' ');
+			}
 		_putchar(92);
 		_putchar('\n');
 		}
 	}
 	else
+	{
 		_putchar('\n');
+	}
 }
 
