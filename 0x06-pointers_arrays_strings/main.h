869648ee148f90d39790b340c70733ca0772f07c
remikeks <keksclusive@gmail.com>
2022-04-24 08:10:56 -0700

    Header file

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+char *strcat(char *dest, char *src);
+char *strncat(char *dest, char *src, int n);
+char *strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
+char *rot13(char *str);
+void print_number(int n);
+int _putchar(char c);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+#endif /*MAIN_H*/
